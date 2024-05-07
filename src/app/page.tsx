b84b5fd29f84b6b2d3b8ff9bf7229a8aa099929e
Bangc121 <wjdrlalf17@gmail.com>
2024-05-08 00:41:58 +0900

    data fetching

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,39 @@
 import Login from "@/components/Login";
+import { client } from "../../sanity/lib/client";
 
-export default function Home() {
+type post = {
+  title: string;
+  content: string;
+  thumbnail: string;
+};
+
+async function getData() {
+  const query = `*[_type == "post"] {
+    title,
+    content,
+    thumbnail
+  }`;
+
+  const data = await client.fetch(query);
+
+  return data;
+}
+
+export default async function Home() {
+  const data: post[] = await getData();
+
+  console.log("data", data);
   return (
     <>
+      <h1>ewfefef</h1>
+      {data.map((post) => {
+        return (
+          <div key={post.title}>
+            <h1>{post.title}</h1>
+            <p>{post.content}</p>
+          </div>
+        );
+      })}
       <Login />
     </>
   );
